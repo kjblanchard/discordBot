bba457233d6105583c546d338ddf766cf4849297
Kevin Blanchard <blanchardkevinj@gmail.com>
2021-11-19 14:58:12 -0500

    Removed the typing handler

--- dotnetBot/typing/TypingHandler.cs
+++ dotnetBot/typing/TypingHandler.cs
@@ -13,15 +13,15 @@ namespace discordBot.typing
 
         public void SetupTypingEvents()
         {
-            _client.TypingStarted += async (sender, args) =>
-            {
-                var nickname = args.Guild.GetMemberAsync(args.User.Id).Result.Nickname;
-                if (args.User.Id == Constants.ConfigurationFile.KevinsUserId && args.Channel.Id == Constants.ConfigurationFile.BotChannelId)
-                {
-                    await _client.SendMessageAsync(args.Channel, $"Kevin, quit typing you lil loser");
-                }
+            //_client.TypingStarted += async (sender, args) =>
+            //{
+            //    var nickname = args.Guild.GetMemberAsync(args.User.Id).Result.Nickname;
+            //    if (args.User.Id == Constants.ConfigurationFile.KevinsUserId && args.Channel.Id == Constants.ConfigurationFile.BotChannelId)
+            //    {
+            //        await _client.SendMessageAsync(args.Channel, $"Kevin, quit typing you lil loser");
+            //    }
 
-            };
+            //};
         }
     }
 }
